e89acf0c7e25e12173a9af9ded34458dd3275b78
Macblack131 <mak.gorohov220v@gmail.com>
2024-07-22 12:52:38 +0300

    first commit

---
+++ Mac_NamePlateAuras.lua
@@ -0,0 +1,9 @@
+Mac_NamePlateAurasMixin = {}
+
+function Mac_NamePlateAurasMixin:OnLoad()
+  self:RegisterEvent("NAME_PLATE_CREATED")
+end
+
+function Mac_NamePlateAurasMixin:OnEvent(event, ...)
+
+end
